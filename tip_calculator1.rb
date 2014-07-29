a92d783d96932bb279b6a2a6bc95bb707533674e
Daniel Cordell <daniel.cordell@masintel.com>
2014-07-29 12:08:10 -0700

    fix variable inconsistencies

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -7,7 +7,7 @@ meal_with_tax = meal + tax_value
 tip_value = meal * tip/100
 total_cost = meal_with_tax + tip_value
 
-print "Meal: $%.2f.\n" % meal_cost
-print "Tax: $%.2f.\n" % [tax_percent, tax_value]
-print "Tip: $%.2f.\n" % [tip_percent, tip_value]
+print "Meal: $%.2f.\n" % meal
+print "Tax: $%.2f.\n" % [tax, tax_value]
+print "Tip: $%.2f.\n" % [tip, tip_value]
 print "Total: $%.2f.\n" % total_cost
