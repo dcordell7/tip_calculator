f667a73ac5bf22f3610278604c803beaea428171
Daniel Cordell <daniel.cordell@masintel.com>
2014-08-01 11:30:00 -0700

    upgrade calculator

---
+++ tip_calculator2.rb
@@ -0,0 +1,16 @@
+puts "Enter meal cost: $ "
+meal = Float(gets)
+puts "Enter tax percentage: "
+tax = Float(gets)
+puts "Enter tip percentage: "
+tip = Float(gets)
+
+tax_value = meal * tax/100
+meal_with_tax = meal + tax_value
+tip_value = meal * tip/100
+total_cost = meal_with_tax + tip_value
+
+print "Meal: $%.2f.\n" % meal
+print "Tax: $%.2f.\n" % [tax, tax_value]
+print "Tip: $%.2f.\n" % [tip, tip_value]
+print "Total: $%.2f.\n" % total_cost
